06437872fef055d6a42f24d712631a7926e7e064
jgoode07 <joegoode.07@gmail.com>
2025-08-15 15:30:18 -0700

    Add DOM elements, e.listeners
    Tested, fully functioning

--- app.js
+++ app.js
@@ -12,3 +12,28 @@ function select(element) {
 function onEvent(event, selector, callback) {
   return selector.addEventListener(event, callback);
 }
+
+/*--------------- DOM Elements ---------------*/
+const box1 = getElement("box1");
+const box2 = getElement("box2");
+const box3 = getElement("box3");
+
+const btnColour = select(".btn-colour");
+const btnCircle = select(".btn-circle");
+const btnHide   = select(".btn-hide");
+
+/*--------------- Event Listeners ---------------*/
+/* Change Colour */
+onEvent("click", btnColour, function () {
+  box1.classList.add("change-colour");
+});
+
+/* Change Shape to Circle */
+onEvent("click", btnCircle, function () {
+  box2.classList.add("change-shape");
+});
+
+/* Toggle Full Opacity (Hidden) */
+onEvent("click", btnHide, function () {
+  box3.classList.add("hide-box");
+});
